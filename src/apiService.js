da67f4955beab9f1948a185c53ff636b125ee13e
andriy-lanyk <LAM1988@ukr.net>
2021-06-05 21:14:47 +0300

    create gallery

--- src/apiService.js
+++ src/apiService.js
@@ -2,9 +2,9 @@ const API_KEY = '21457402-b4837d779b51461007fdd5443';
 const URL = `https://pixabay.com/api/`;
 
 export default {
-  getPhoto: function () {
+  getPhoto: function (query) {
     return fetch(
-      `${URL}?image_type=photo&orientation=horizontal&q=${'cat'}&page=${1}&per_page=12&key=${API_KEY}`,
+      `${URL}?image_type=photo&orientation=horizontal&q=${query}&page=${1}&per_page=12&key=${API_KEY}`,
     ).then(res => {
       return res.json();
     });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+import debounce from 'lodash.debounce';
 import './sass/main.scss';
 import apiObject from './apiService.js';
 import createFormTpl from './tamplete/createForm.hbs';
+import createPhotoListTpl from './tamplete/createPhotoList.hbs';
+import createGalleryUlTpl from './tamplete/createGalleryUl.hbs';
 
+document.body.insertAdjacentHTML('afterbegin', createGalleryUlTpl());
 document.body.insertAdjacentHTML('afterbegin', createFormTpl());
 
-apiObject.getPhoto().then(res => {
-  console.log('res: ', res);
-});
+const gallery = document.querySelector('.gallery');
+const input = document.querySelector('[name="query"]');
+const fragment = document.createDocumentFragment();
+
+input.addEventListener('input', debounce(getQuery, 500));
+
+function getQuery(e) {
+  let query = e.target.value;
+
+  apiObject.getPhoto(query).then(({ hits }) => {
+    gallery.innerHTML = createPhotoListTpl(hits);
+  });
+}
