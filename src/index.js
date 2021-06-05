499a0fa3974879eca24ff4ee10281cbc44c74d8e
andriy-lanyk <LAM1988@ukr.net>
2021-06-05 18:04:49 +0300

    add form in html file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './sass/main.scss';
 import apiObject from './apiService.js';
+import createFormTpl from './tamplete/createForm.hbs';
+
+document.body.insertAdjacentHTML('afterbegin', createFormTpl());
 
 apiObject.getPhoto().then(res => {
   console.log('res: ', res);
